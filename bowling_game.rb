c0d2df4b4ac91592c5fca0e3c88a4645f2dafe2b
Kei Nishina <k247loveke@gmail.com>
2015-09-26 10:15:51 +0859

    end of lecture

--- bowling_game.rb
+++ bowling_game.rb
@@ -3,15 +3,14 @@ require_relative 'frame'
 class BowlingGame
   def initialize
     @spare_frame = nil
-    @strikes = []
+    @bonus_frames = []
     @frames = [ Frame.new ]
   end
 
   def record_shot(pins)
     frame = @frames.last
     frame.record_shot(pins)
-    calc_spare_bonus(pins)
-    calc_strike_bonus(pins)
+    calc_bonus(pins)
     if frame.finished?
       @frames << Frame.new
     end
@@ -27,32 +26,17 @@ class BowlingGame
 
   private
 
-  def calc_spare_bonus(pins)
-    if @spare_frame && @spare_frame.need_bonus?
-      @spare_frame.add_bonus(pins)
-    end
-    if @frames.last.spare?
-      @spare_frame = @frames.last
+  def calc_bonus(pins)
+    distribute_bonus(pins)
+    if @frames.last.need_bonus?
+      @bonus_frames << @frames.last
     end
   end
 
-  def calc_strike_bonus(pins)
-    add_strike_bonus(pins)
-    if @frames.last.strike?
-      recognize_strike_bonus
+  def distribute_bonus(pins)
+    @bonus_frames.select(&:need_bonus?).each do |frame|
+      frame.add_bonus(pins)
     end
   end
-
-    def recognize_strike_bonus
-      @strikes << @frames.last
-    end
-
-    def add_strike_bonus(pins)
-      @strikes.each do | strike | 
-        if strike.need_bonus?
-          strike.add_bonus(pins)
-        end
-      end
-    end
 
 end
